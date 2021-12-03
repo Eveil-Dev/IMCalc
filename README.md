736a518ca2a6be60d5f907cbdacc49e7c535c050
Evelyn Monteiro <evelynsilmonteiro@gmail.com>
2021-12-02 22:12:35 -0300

    IMCalc 2.0

---
+++ README.md
@@ -0,0 +1,51 @@
+<h1 align="center">IMCalc</h1>
+
+<h2 align="center">
+    <img alt="Decbase" title="Decbase" src="images/Logo.png" height="150" />
+</h2>
+
+<p align="center">A very efficient BMI calculator</p>
+
+<h4 align="center"> 
+	🚧  Finished ! 🚧
+</h4>
+
+<br>
+
+## 💻 About the project
+
+<br>
+
+BMICalc is a very efficient and to-the-point BMI calculator where you can quickly check if you have a good Body Mass Index just by entering your height and weight.
+
+<br>
+
+## 🎨 Interface
+
+<br>
+
+### Desktop
+
+<p>
+  <img alt="Mobile IMCalc Interface" title="MobileIMCalc" src="images/IMCalc-Desktop.gif" width="700px">
+</p>
+
+<br>
+
+### Mobile
+
+<p>
+    <img alt="Mobile IMCalc Interface" title="MobileIMCalc" src="images/IMCalc-Mobile.gif" width="300px" height="515">
+    <img alt="Mobile IMCalc Interface" title="MobileIMCalc" src="images/IMCalc-Image-Mobile.png" width="300px" height="515">
+</p>
+
+<br>
+
+<br>
+
+## 👩🏻 Autor
+
+Made with 💜 by Evelyn Monteiro 👋
+<br>
+
+[![LinkedIn Badge](https://img.shields.io/badge/-Evelyn_Monteiro-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/evelyn-monteiro-5b61981b3/)](https://www.linkedin.com/in/evelyn-monteiro-5b61981b3/)
--- app.js
+++ app.js
@@ -1,54 +1,125 @@
-const inputName = document.getElementById("name");
-const inputHeight = document.getElementById("height");
-const inputWeight = document.getElementById("weight");
-const calcBtn = document.getElementById("calc");
-const resultDiv = document.getElementById("result")
+const buttonCalc = document.getElementById("calc");
+const buttonAgain = document.getElementById("again");
+const bmiCalcContainer = document.querySelector(".bmi-calc-container");
+const bmiResultContainer = document.querySelector(".bmi-result-container");
+const bmi = document.getElementById("bmi");
+const description = document.getElementById("description");
 
-calcBtn.addEventListener('click', function(){
+let mob_view = window.matchMedia("(max-width: 600px)");
+let desk_view = window.matchMedia("(min-width: 601px)");
 
-    try{
-        validateFields();
-        result = calcImc();
-        imcText = imcDefinition(result);
-        resultDiv.innerHTML = `Olá ${inputName.value}, seu IMC é de ${result} <br> e você está ${imcText}`;
-        inputName.value = "";
-        inputHeight.value = "";
-        inputWeight.value = "";
+const arrowsHeightInput = document.querySelectorAll(".height-input .arrow");
+const heightInput = document.getElementById("height");
 
-    }catch(error){
-        alert(error.message);
+const arrowsWeightInput = document.querySelectorAll(".weight-input .arrow");
+const weightInput = document.getElementById("weight");
+
+// -> CALC IMC
+
+function calcIMC() {
+  let weightInputValue = parseFloat(weightInput.value.replace(",", "."));
+  let heightInputValue = parseFloat(heightInput.value.replace(",", "."));
+
+  let result = 0;
+  if (heightInputValue > 1) {
+    result = weightInputValue / (heightInputValue * heightInputValue);
+  } else {
+    result =
+      (weightInputValue * 1000) /
+      (heightInputValue * 100 * (heightInputValue * 100));
+  }
+
+  result = Math.round(result * 100) / 100;
+  return result;
+}
+
+// -> IMC DEFINITION
+
+function imcDefinition(result) {
+  let imcDefinition;
+
+  if (result < 18.5) {
+    imcDefinition = "Under body weight";
+  } else if (result >= 18.5 && result <= 24.9) {
+    imcDefinition = "Normal body weight";
+  } else if (result >= 25 && result <= 29.9) {
+    imcDefinition = "You are over your body weight";
+  } else if (result >= 30 && result <= 34.9) {
+    imcDefinition = "Grade I obesity";
+  } else if (result >= 35 && result <= 39.9) {
+    imcDefinition = "Grade II obesity";
+  } else {
+    imcDefinition = "Morbid obesity";
+  }
+
+  return imcDefinition;
+}
+
+// -> VALIDATE FIELDS
+
+function validateFields() {
+  if (heightInput.value == "0" || weightInput.value == "0") {
+    throw new Error("Por favor preencha todos os campos");
+  }
+}
+
+// -> CALC EVENT LISTENER
+
+buttonCalc.addEventListener("click", function () {
+  try {
+    validateFields();
+    result = calcIMC();
+    imcText = imcDefinition(result);
+    bmi.innerHTML = result;
+    description.innerHTML = `${imcText}`;
+    heightInput.value = "0";
+    weightInput.value = "0";
+    if (mob_view.matches) {
+      bmiCalcContainer.classList.add("hidden-calc-container");
+      bmiResultContainer.classList.add("show-result-mobile");
     }
-
+  } catch (error) {
+    alert(error.message);
+  }
 });
 
-function validateFields(){
-    if(inputName.value == "" || inputHeight.value == "" || inputWeight.value == ""){
-        throw new Error("Por favor preencha todos os campos");
-    }
-}
+buttonAgain.addEventListener("click", function () {
+  bmiCalcContainer.classList.remove("hidden-calc-container");
+  bmiResultContainer.classList.remove("show-result-mobile");
+});
 
-function calcImc(){
-    let result = inputWeight.value / (inputHeight.value * inputHeight.value);
-    result = Math.round(result * 100) / 100;
-    return result;
-}
+// -> INPUT HEIGHT
 
-function imcDefinition(result){
-    let imcDefinition;
-
-    if(result < 18.5){
-        imcDefinition = "abaixo do peso";
-    }else if(result >= 18.5 && result <= 24.9){
-        imcDefinition = "peso normal";
-    }else if(result >= 25 && result <= 29.9){
-        imcDefinition = "acima do peso";
-    }else if(result >= 30 && result <= 34.9){
-        imcDefinition = "com obesidade grau I"
-    }else if(result >= 35 && result <= 39.9){
-        imcDefinition = "com obesidade grau II"
-    }else{
-        imcDefinition = "com obesidade morbida"
+arrowsHeightInput.forEach(function (arrow) {
+  arrow.addEventListener("click", function (e) {
+    let currentHeight = parseFloat(heightInput.value);
+    const targetArrow = e.currentTarget;
+    if (targetArrow.classList.contains("increase")) {
+      currentHeight = currentHeight + 0.01;
+    } else if (targetArrow.classList.contains("decrease")) {
+      if (currentHeight > 0) {
+        currentHeight = currentHeight - 0.01;
+      }
     }
 
-    return imcDefinition;
-}
+    heightInput.value = currentHeight.toFixed(2);
+  });
+});
+
+// -> INPUT WEIGHT
+
+arrowsWeightInput.forEach(function (arrow) {
+  arrow.addEventListener("click", function (e) {
+    let currentWeight = parseFloat(weightInput.value);
+    const targetArrow = e.currentTarget;
+    if (targetArrow.classList.contains("increase")) {
+      currentWeight = currentWeight + 1;
+    } else if (targetArrow.classList.contains("decrease")) {
+      if (currentWeight > 0) {
+        currentWeight = currentWeight - 1;
+      }
+    }
+
+    weightInput.value = currentWeight;
+  });
+});
